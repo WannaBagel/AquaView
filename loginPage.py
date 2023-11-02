3958d80ec118bb8bc1f885c7c7848932bde4565f
Emma-Reiff <98106194+Emma-Reiff@users.noreply.github.com>
2023-11-01 22:26:28 -0500

    tk window opens

--- loginPage.py
+++ loginPage.py
@@ -1,2 +1,16 @@
 from tkinter import *
-from PIL import ImageTK, image  # pip install pillow
+from PIL import ImageTk, Image  # pip install pillow
+
+class LoginPage:
+    def __init__(self, window):
+        self.window = window
+        self.window.geometry('1166x718')
+
+def page():
+    window = Tk()
+    LoginPage(window)
+    window.mainloop()
+
+
+if __name__ == '__main__':
+    page()
